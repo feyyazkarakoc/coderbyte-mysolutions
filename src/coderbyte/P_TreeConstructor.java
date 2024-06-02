8c50594065af6a3c0c443de63015ed8744cee5d4
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-06-03 01:52:12 +0300

    TreeConstructor

---
+++ src/coderbyte/P_TreeConstructor.java
@@ -0,0 +1,108 @@
+package coderbyte;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class P_TreeConstructor {
+
+    public static void main(String[] args) {
+
+        String[] strArr =  {"(1,2)", "(3,2)", "(2,12)", "(5,2)"};
+
+
+
+        System.out.println(TreeConstructor(strArr));
+
+
+
+    }
+
+
+    public static String TreeConstructor(String[] strArr) {
+
+
+
+
+
+        List<Integer> list = new ArrayList<>();
+        List<Integer> list1 = new ArrayList<>();
+
+
+        for (String w : strArr) {
+
+            String trimmed = w.replace("(", "").replace(")", "");
+            String[] parts = trimmed.split(",");
+
+
+
+            String firstComponent = parts[0];
+            list.add(Integer.valueOf(firstComponent));
+
+            String secondComponent = parts[1];
+            list1.add(Integer.valueOf(secondComponent));
+
+
+
+        }
+
+
+        int counterFirst = 1;
+        int counterSecond = 1;
+        boolean first = true;
+        boolean second = true;
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+
+
+
+                if (list.get(i).equals(list.get(j))) {
+                    counterFirst++;
+
+                    if (!(list1.get(i).equals(list1.get(j)))) {
+                        first = false;
+                        break;
+                    }
+
+
+                    if (counterFirst > 2) {
+                        first = false;
+                        break;
+                    }
+
+
+                }
+
+                if (list1.get(i).equals(list1.get(j))) {
+                    counterSecond++;
+                    if (counterSecond > 2) {
+                        second = false;
+                        break;
+                    }
+
+                }
+
+
+            }
+            counterFirst = 1;
+            counterSecond = 1;
+
+        }
+
+
+        if (first == false || second == false) {
+
+            return "false";
+        }
+
+        return "true";
+
+
+
+
+
+    }
+
+
+}
