1603d5654b875b685115a9f5459f3b778739e54b
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-06-14 00:04:25 +0300

    commit

--- src/coderbyte/P_MinWindowSubstring.java
+++ src/coderbyte/P_MinWindowSubstring.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class P_MinWindowSubstring {
 
 
-    public static String MinWindowSubstring(String[] strArr){
+    public static String MinWindowSubstring(String[] strArr) {
 
         String n = strArr[0];
         String k = strArr[1];
@@ -13,18 +13,18 @@ public class P_MinWindowSubstring {
         int minLength = Integer.MAX_VALUE;
         String shortestSubstring = "";
 
-        for (int i = 0; i < n.length() ; i++) {
+        for (int i = 0; i < n.length(); i++) {
             StringBuilder match = new StringBuilder(k);
             for (int j = i; j < n.length(); j++) {
                 if (match.toString().contains(String.valueOf(n.charAt(j)))) {
-                int idx = match.indexOf(String.valueOf(n.charAt(j)));
-                match.replace(idx,idx+1, "");
+                    int idx = match.indexOf(String.valueOf(n.charAt(j)));
+                    match.replace(idx, idx + 1, "");
                 }
 
                 if (match.length() == 0) {
-                    if (j - i < minLength){
+                    if (j - i < minLength) {
                         minLength = j - i;
-                        shortestSubstring = n.substring(i,j+1);
+                        shortestSubstring = n.substring(i, j + 1);
                     }
                     break;
                 }
@@ -33,7 +33,10 @@ public class P_MinWindowSubstring {
         return shortestSubstring;
     }
 
-    public static void main (String[] args) {
+
+
+
+    public static void main(String[] args) {
         String[] strArr = {"aaabaaddae", "aed"};
         System.out.print(MinWindowSubstring(strArr));
     }
@@ -41,6 +44,86 @@ public class P_MinWindowSubstring {
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+  /*  public static String MinWindowSubstring(String[] strArr) {
+
+        String n = strArr[0];
+        String k = strArr[1];
+
+        int minLength = Integer.MAX_VALUE;
+        String shortestSubstring = "";
+
+        for (int i = 0; i < n.length(); i++) {
+            StringBuilder match = new StringBuilder(k);
+            for (int j = i; j < n.length(); j++) {
+                if (match.toString().contains(String.valueOf(n.charAt(j)))) {
+                    int idx = match.indexOf(String.valueOf(n.charAt(j)));
+                    match.replace(idx, idx + 1, "");
+                }
+
+                if (match.length() == 0) {
+                    if (j - i < minLength) {
+                        minLength = j - i;
+                        shortestSubstring = n.substring(i, j + 1);
+                    }
+
+                    break;
+                }
+
+            }
+
+        }
+
+        return shortestSubstring;
+    }*/
+
+
+   /* public static String MinWindowSubstring(String[] strArr) {
+        String n = strArr[0];
+        String k = strArr[1];
+
+        int minLength = Integer.MAX_VALUE;
+        String shortestSubstring = "";
+
+        for (int i = 0; i < n.length(); i++) {
+            StringBuilder match = new StringBuilder(k);
+
+            for (int j = i; j < n.length(); j++) {
+                if (match.toString().contains(String.valueOf(n.charAt(j)))) {
+                    int idx = match.indexOf(String.valueOf(n.charAt(j)));
+                    match.replace(idx, idx + 1, "");
+                }
+
+                if (match.length() == 0) {
+
+                    if (j - i < minLength) {
+
+                        minLength = j - i;
+                        shortestSubstring = n.substring(i, j + 1);
+
+                    }
+                    break;
+
+                }
+
+
+            }
+
+        }
+
+
+        return shortestSubstring;
+    }*/
 
 
 }
