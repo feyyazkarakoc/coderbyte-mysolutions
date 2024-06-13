53c03a08b481b216b0a9c56a84413c824cb44098
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2024-06-13 23:40:29 +0300

    MinWindowSubstring

---
+++ src/coderbyte/P_MinWindowSubstring.java
@@ -0,0 +1,54 @@
+package coderbyte;
+
+import java.util.*;
+
+public class P_MinWindowSubstring {
+
+
+    public static String MinWindowSubstring(String[] strArr){
+
+        String n = strArr[0];
+        String k = strArr[1];
+
+        int minLength = Integer.MAX_VALUE;
+        String shortestSubstring = "";
+
+        for (int i = 0; i < n.length() ; i++) {
+            StringBuilder match = new StringBuilder(k);
+            for (int j = i; j < n.length(); j++) {
+                if (match.toString().contains(String.valueOf(n.charAt(j)))) {
+                int idx = match.indexOf(String.valueOf(n.charAt(j)));
+                match.replace(idx,idx+1, "");
+                }
+
+                if (match.length() == 0) {
+                    if (j - i < minLength){
+                        minLength = j - i;
+                        shortestSubstring = n.substring(i,j+1);
+                    }
+                    break;
+                }
+            }
+        }
+        return shortestSubstring;
+    }
+
+    public static void main (String[] args) {
+        String[] strArr = {"aaabaaddae", "aed"};
+        System.out.print(MinWindowSubstring(strArr));
+    }
+
+
+
+
+
+
+}
+
+
+
+        
+        
+        
+
+
