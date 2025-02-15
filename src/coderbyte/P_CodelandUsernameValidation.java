22caa1c932f39be8b2ce0b6f0e8e533f1f5d9d19
Feyyaz Karakoç <fyyzkrkc@gmail.com>
2025-02-16 01:25:05 +0300

    solution

--- src/coderbyte/P_CodelandUsernameValidation.java
+++ src/coderbyte/P_CodelandUsernameValidation.java
@@ -2,7 +2,15 @@ package coderbyte;
 
 public class P_CodelandUsernameValidation {
 
-    public static String CodelandUsernameValidation(String str) {
+    public static void main(String[] args) {
+        System.out.println(CodelandUsernameValidation("u__hello_world123"));
+
+    }
+
+
+
+    // Solution 1:
+    /*public static String CodelandUsernameValidation(String str) {
 
 
 
@@ -18,10 +26,18 @@ public class P_CodelandUsernameValidation {
         }
 
         return "false";
+    }*/
+
+
+
+    // Solution 2:
+    public static String CodelandUsernameValidation(String str) {
+
+        if(str.endsWith("-")||(str.length()<4||str.length()>25)||str.substring(0,1).replaceAll("[^^A-Za-z]","").length()==1||str.replaceAll("[^A-Za-z0-9_]","").length()==0){
+            return "false";
+        }
+
+        return "true";
     }
 
-    public static void main(String[] args) {
-        System.out.println(CodelandUsernameValidation("u__hello_world123"));
-
-    }
 }
--- src/coderbyte/P_FirstReverse.java
+++ src/coderbyte/P_FirstReverse.java
@@ -7,7 +7,11 @@ public class P_FirstReverse {
         System.out.println(FirstReverse("Coderbyte"));
     }
 
-    public static String FirstReverse(String str) {
+
+
+
+    // solution 1:
+    /*public static String FirstReverse(String str) {
 
         String reversedStr = "";
         String[] strArr = str.split("");
@@ -18,6 +22,58 @@ public class P_FirstReverse {
         }
         return reversedStr;
 
+    }*/
+
+
+
+    // solution 2 :
+
+    /*public static String FirstReverse(String str) {
+        StringBuilder sb = new StringBuilder();
+        for(int i = str.length()-1; i>=0; i--){
+            sb.append("").append(str.charAt(i));
+        }
+
+        return sb.toString();
+    }*/
+
+
+    // solution 3 :
+    /*public static String FirstReverse(String str) {
+
+        String[] strArr = str.split("");
+        String reversedStr = "";
+        for(int i = strArr.length-1; i>=0;i--){
+            reversedStr+=strArr[i];
+        }
+        return reversedStr;
+
+    }*/
+
+
+
+    // solution 4 :
+    /*public static String FirstReverse(String str) {
+        return new StringBuilder(str).reverse().toString();
+    }*/
+
+
+
+
+
+    /*public static String FirstReverse(String str) {
+
+        String reversedStr="";
+        for(int i = str.length()-1; i>=0;i--){
+            reversedStr+=str.charAt(i)+"";
+        }
+        return reversedStr;
+    }*/
+
+
+
+    public static String FirstReverse(String str) {
+        return new StringBuilder(str).reverse().toString();
     }
 
 
